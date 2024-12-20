f5c5ae3796b22563322a54bc5242fbb13996c1af
Ronaldo9518 <rsalazar00171@ufide.ac.cr>
2024-12-19 22:12:24 -0600

    1234

--- SuperMarket/src/main/java/Login/FromLogin.java
+++ SuperMarket/src/main/java/Login/FromLogin.java
@@ -11,12 +11,12 @@ import javax.swing.JOptionPane;
  *
  * @author salaz
  */
-public class Login extends javax.swing.JFrame {
+public class FromLogin extends javax.swing.JFrame {
 
     /**
      * Creates new form Login
      */
-    public Login() {
+    public FromLogin() {
         initComponents();
     }
 
@@ -119,13 +119,12 @@ public class Login extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
     
-        if(Usuariotxt.getText().equals ("Admin") && Passwordtxt.getText().equals ("1234")) 
+        if(Usuariotxt.getText().equals ("Admin") && new String(Passwordtxt.getPassword()).equals ("1234")) 
         {  new Menu().setVisible(true);
-          this.setVisible(false);
+           this.setVisible(false);
 }  
-          else 
-          JOptionPane.showMessageDialog(null,"Error, Datos Incorrectos.",
-          JOptionPane.ERROR_MESSAGE);
+        else { 
+          JOptionPane.showMessageDialog(null,"Error, Datos Incorrectos.",JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_jButton1ActionPerformed
 
@@ -146,20 +145,21 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FromLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FromLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FromLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(FromLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new FromLogin().setVisible(true);
             }
         });
     }
